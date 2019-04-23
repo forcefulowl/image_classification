b6b096474d663e24c74dc802b53df163a9440ece
forcefulowl <31970467+forcefulowl@users.noreply.github.com>
2019-04-23 06:24:19 -0400

    Update image_utils.py

--- image_utils.py
+++ image_utils.py
@@ -1,5 +1,3 @@
-"""Utilities for ImageNet data preprocessing & prediction decoding.
-"""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
