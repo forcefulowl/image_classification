dd5c2c063906d40a0d661e15b5503d00a6d89ec5
forcefulowl <31970467+forcefulowl@users.noreply.github.com>
2019-04-24 02:11:56 -0400

    Update model_inverted_residual_resnet50.py

--- model_inverted_residual_resnet50.py
+++ model_inverted_residual_resnet50.py
@@ -1,35 +1,32 @@
-from keras import layers
-from keras import backend
-from keras.callbacks import ModelCheckpoint, LearningRateScheduler, TensorBoard, EarlyStopping
-import datetime
-import tensorflow as tf
-from keras.preprocessing.image import ImageDataGenerator
-from keras import optimizers
-from keras.models import Model
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import division
+
+import os
+import warnings
+import numpy as np
+
 from _init_ import correct_pad
-from _init_ import get_submodules_from_kwargs
+
+import image_utils
+from image_utils import decode_predictions
+from image_utils import _obtain_input_shape
+
+from keras.layers import Input
+from keras import layers
+from keras.models import Model
+from keras import backend
 
 
-'''
-backend = None
-layers = None
-models = None
-keras_utils = None
-'''
+def preprocess_input(x, **kwargs):
+
+    return image_utils.preprocess_input(x, mode='tf', **kwargs)
 
 
-start = datetime.datetime.now()
-
-tf.global_variables_initializer()
-
-img_width, img_height = 224, 224
-train_data_dir = "C:\\Users\gavin\Desktop\Train1_M"
-validation_data_dir = "C:\\Users\gavin\Desktop\Test1_M"
-data_dir = 'C:\\Users\gavin\Desktop\whole_M'
-nb_train_samples = 7000
-nb_validation_samples = 3000
-batch_size = 64
-epochs = 200
+# This function is taken from the original tf repo.
+# It ensures that all layers have a channel number that is divisible by 8
+# It can be seen here:
+# https://github.com/tensorflow/models/blob/master/research/slim/nets/mobilenet/mobilenet.py
 
 
 def _make_divisible(v, divisor, min_value=None):
@@ -44,35 +41,46 @@ def _make_divisible(v, divisor, min_value=None):
 
 def MobileNetV2(input_shape=None,
                 alpha=1.0,
-                depth_multiplier=1,
+                include_top=True,
+                weights=None,
                 input_tensor=None,
                 pooling=None,
                 classes=7,
                 **kwargs):
 
-#    global backend, layers, models, keras_utils
-#    backend, layers, models, keras_utils = get_submodules_from_kwargs(kwargs)
+    rows = input_shape[0]
+    cols = input_shape[1]
 
-    if input_shape is None:
-        default_size = 224
+    if rows == cols and rows in [96, 128, 160, 192, 224]:
+        default_size = rows
     else:
-        rows = input_shape[0]
-        cols = input_shape[1]
+        default_size = 224
 
-        if rows == cols and rows in [128, 160, 192, 224]:
-            default_size = rows
-        else:
-            default_size = 224
+    input_shape = _obtain_input_shape(input_shape,
+                                      default_size=default_size,
+                                      min_size=32,
+                                      data_format='channels_last',
+                                      require_flatten=include_top,
+                                      weights=weights)
 
-    input_shape = (default_size, default_size, 3)
-
-    row_axis, col_axis = (0, 1)
+    if backend.image_data_format() == 'channels_last':
+        row_axis, col_axis = (0, 1)
+    else:
+        row_axis, col_axis = (1, 2)
     rows = input_shape[row_axis]
     cols = input_shape[col_axis]
 
-    img_input = layers.Input(shape=input_shape)
+    if input_tensor is None:
+        img_input = layers.Input(shape=input_shape)
+    else:
+        if not backend.is_keras_tensor(input_tensor):
+            img_input = layers.Input(tensor=input_tensor, shape=input_shape)
+        else:
+            img_input = input_tensor
 
-    first_block_filters=_make_divisible(32 * alpha, 8)
+    channel_axis = 1 if backend.image_data_format() == 'channels_first' else -1
+
+    first_block_filters = _make_divisible(32 * alpha, 8)
     x = layers.ZeroPadding2D(padding=correct_pad(backend, img_input, 3),
                              name='Conv1_pad')(img_input)
     x = layers.Conv2D(first_block_filters,
@@ -81,9 +89,11 @@ def MobileNetV2(input_shape=None,
                       padding='valid',
                       use_bias=False,
                       name='Conv1')(x)
-    x = layers.BatchNormalization(
-        epsilon=1e-3, momentum=0.999, name='bn_Conv1')(x)
-    x = layers.ReLU(6., name='Conv1_relu')(x)
+    x = layers.BatchNormalization(axis=channel_axis,
+                                  epsilon=1e-3,
+                                  momentum=0.999,
+                                  name='bn_Conv1')(x)
+    x = layers.Activation('relu')(x)
 
     x = _inverted_res_block(x, filters=16, alpha=alpha, stride=1,
                             expansion=1, block_id=0)
@@ -126,6 +136,9 @@ def MobileNetV2(input_shape=None,
     x = _inverted_res_block(x, filters=320, alpha=alpha, stride=1,
                             expansion=6, block_id=16)
 
+    # no alpha applied to last conv as stated in the paper:
+    # if the width multiplier is greater than 1 we
+    # increase the number of output channels
     if alpha > 1.0:
         last_block_filters = _make_divisible(1280 * alpha, 8)
     else:
@@ -135,43 +148,53 @@ def MobileNetV2(input_shape=None,
                       kernel_size=1,
                       use_bias=False,
                       name='Conv_1')(x)
-    x = layers.BatchNormalization(epsilon=1e-3,
+    x = layers.BatchNormalization(axis=channel_axis,
+                                  epsilon=1e-3,
                                   momentum=0.999,
                                   name='Conv_1_bn')(x)
-    x = layers.ReLU(6., name='out_relu')(x)
+    x = layers.Activation('relu')(x)
 
-    if pooling == 'avg':
+    if include_top:
         x = layers.GlobalAveragePooling2D()(x)
-    elif pooling == 'max':
-        x = layers.GlobalMaxPooling2D()(x)
+        x = layers.Dense(classes, activation='softmax',
+                         use_bias=True, name='Logits')(x)
+    else:
+        if pooling == 'avg':
+            x = layers.GlobalAveragePooling2D()(x)
+        elif pooling == 'max':
+            x = layers.GlobalMaxPooling2D()(x)
 
     inputs = img_input
+    # Create model.
+    model = Model(inputs, x,
+                         name='mobilenetv2_%0.2f_%s' % (alpha, rows))
 
-    model = Model(inputs, x)
 
     return model
 
 
-
 def _inverted_res_block(inputs, expansion, stride, alpha, filters, block_id):
-    in_channels = backend.int_shape(inputs)[-1]
+    channel_axis = 1 if backend.image_data_format() == 'channels_first' else -1
+
+    in_channels = backend.int_shape(inputs)[channel_axis]
     pointwise_conv_filters = int(filters * alpha)
     pointwise_filters = _make_divisible(pointwise_conv_filters, 8)
     x = inputs
     prefix = 'block_{}_'.format(block_id)
 
     if block_id:
+        # Expand
         x = layers.Conv2D(expansion * in_channels,
                           kernel_size=1,
                           padding='same',
                           use_bias=False,
                           activation=None,
                           name=prefix + 'expand')(x)
-        x = layers.BatchNormalization(epsilon=1e-3,
+        x = layers.BatchNormalization(axis=channel_axis,
+                                      epsilon=1e-3,
                                       momentum=0.999,
                                       name=prefix + 'expand_BN')(x)
-        x = layers.ReLU(6., name = prefix + 'expand_relu')(x)
-
+        x = layers.Activation('relu')(x)
     else:
         prefix = 'expanded_conv_'
 
@@ -179,17 +202,18 @@ def _inverted_res_block(inputs, expansion, stride, alpha, filters, block_id):
     if stride == 2:
         x = layers.ZeroPadding2D(padding=correct_pad(backend, x, 3),
                                  name=prefix + 'pad')(x)
-
     x = layers.DepthwiseConv2D(kernel_size=3,
                                strides=stride,
                                activation=None,
                                use_bias=False,
                                padding='same' if stride == 1 else 'valid',
                                name=prefix + 'depthwise')(x)
-    x = layers.BatchNormalization(epsilon=1e-3,
+    x = layers.BatchNormalization(axis=channel_axis,
+                                  epsilon=1e-3,
                                   momentum=0.999,
                                   name=prefix + 'depthwise_BN')(x)
-    x = layers.ReLU(6)(x)
+
+    x = layers.Activation('relu')(x)
 
     # Project
     x = layers.Conv2D(pointwise_filters,
@@ -197,67 +221,12 @@ def _inverted_res_block(inputs, expansion, stride, alpha, filters, block_id):
                       padding='same',
                       use_bias=False,
                       activation=None,
-                      name = prefix + 'project')(x)
-    x = layers.BatchNormalization(
-        epsilon=1e-3, momentum=0.999, name = prefix + 'project_BN')(x)
+                      name=prefix + 'project')(x)
+    x = layers.BatchNormalization(axis=channel_axis,
+                                  epsilon=1e-3,
+                                  momentum=0.999,
+                                  name=prefix + 'project_BN')(x)
 
     if in_channels == pointwise_filters and stride == 1:
         return layers.Add(name=prefix + 'add')([inputs, x])
     return x
-
-
-model = MobileNetV2(input_shape=(img_width, img_height, 3), pooling='avg')
-
-x = model.output
-
-x = layers.Dense(7, activation='softmax',
-                 use_bias=True, name='Logits')(x)
-
-model_final = Model(input=model.input, output=x)
-model.summary()
-
-model_final.compile(loss="categorical_crossentropy", optimizer=optimizers.SGD(lr=0.1, momentum=0.9), metrics=["accuracy"])
-
-train_datagen = ImageDataGenerator(
-    rescale=1./255,
-    horizontal_flip=True,
-    fill_mode="nearest",
-    zoom_range=0.3,
-    width_shift_range=0.3,
-    height_shift_range=0.3,
-    rotation_range=30,
-    validation_split=0.3)
-
-train_generator = train_datagen.flow_from_directory(
-    data_dir,
-    target_size=(img_width, img_height),
-    batch_size=batch_size,
-    class_mode="categorical",
-    subset='training',
-    shuffle=True)
-
-validation_generator = train_datagen.flow_from_directory(
-    data_dir,
-    target_size=(img_width, img_height),
-    batch_size=batch_size,
-    class_mode="categorical",
-    subset='validation',
-    shuffle=True)
-
-# early_stop = EarlyStopping(monitor='val_loss', patience=10, verbose=1)
-
-# Train the model
-model_final.fit_generator(
-    train_generator,
-    steps_per_epoch=7000//batch_size,
-    epochs=epochs,
-    validation_data=validation_generator,
-    validation_steps=3000//batch_size,
-    class_weight='auto',
-    shuffle=True,
-    callbacks=[TensorBoard(log_dir='mytensorboard')])
-#   callbacks=[early_stop],
-
-end = datetime.datetime.now()
-print(end-start)
-model_final.save_weights('C:\\Users\gavin\Desktop\model4.h5')
