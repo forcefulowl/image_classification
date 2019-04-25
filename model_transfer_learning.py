021de9db4096b02cb83eb7b210e508399e5395e5
forcefulowl <31970467+forcefulowl@users.noreply.github.com>
2019-04-25 05:46:08 -0400

    Update model_transfer_learning.py

--- model_transfer_learning.py
+++ model_transfer_learning.py
@@ -1,20 +1,33 @@
-from keras import applications
+import keras
 from keras.preprocessing.image import ImageDataGenerator
 from keras import optimizers
 from keras import layers
-from keras.models import Sequential, Model
-from keras.layers import Dropout, Flatten, Dense, BatchNormalization, Activation
-from keras import regularizers
-from keras import backend as k
-from keras.callbacks import ModelCheckpoint, LearningRateScheduler, TensorBoard, EarlyStopping
+from keras.models import  Model
 import datetime
 import tensorflow as tf
 from keras.backend import manual_variable_initialization
 
+import gconv
+import model_separable_dw_resnet50
+import model_separable_dw_vgg16
+import mobile_net2_revised
+import new_revised_resnet50
+import new_revised_vgg16
+import why_cant_import
+
+
+
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+session = tf.InteractiveSession(config=config)
+
 
 start = datetime.datetime.now()
 
+
+
 tf.global_variables_initializer()
+keras.initializers.glorot_normal(seed=None)
 
 img_width, img_height = 224, 224
 train_data_dir = "C:\\Users\gavin\Desktop\Train1_M"
@@ -22,11 +35,11 @@ validation_data_dir = "C:\\Users\gavin\Desktop\Test1_M"
 data_dir = 'C:\\Users\gavin\Desktop\whole_M'
 nb_train_samples = 7000
 nb_validation_samples = 3000
-batch_size = 64
+batch_size = 8
 epochs = 100
 
 
-model = applications.MobileNet(weights='imagenet', include_top=False, input_shape=(img_width, img_height, 3),
+model = model_separable_dw_resnet50.MobileNet(weights=None, include_top=False, input_shape=(img_width, img_height, 3),
                                pooling='avg')
 
 
@@ -35,36 +48,16 @@ model = applications.MobileNet(weights='imagenet', include_top=False, input_shap
 
 x = model.output
 
-'''
-
-# x = Flatten()(x)
-x = Dropout(1e-3)(x)
-x = Dense(1024)(x)
-x = BatchNormalization()(x)
-x = Activation('relu')(x)
-x = Dropout(0.5)(x)
-x = Dense(1024)(x)
-x = BatchNormalization()(x)
-x = Activation('relu')(x)
-x = Dropout(0.5)(x)
-predictions = Dense(7, activation="softmax")(x)
-'''
-
-shape = (1, 1, 1024)
-x = layers.Reshape(shape)(x)
-x = layers.Dropout(1e-3)(x)
-x = layers.Conv2D(7, (1, 1),
-                  padding='same',
-                  kernel_initializer='he_uniform')(x)
-x = layers.Activation('softmax')(x)
-x = layers.Reshape((7,))(x)
+x = layers.Dense(7, activation='softmax')(x)
 
 # creating the final model
-model_final = Model(input=model.input, output=x)
+model_final = Model(inputs=model.input, output=x)
+
+model_final.summary()
 
 # compile the model
-model_final.compile(loss="categorical_crossentropy", optimizer=optimizers.SGD(lr=0.001, momentum=0.9), metrics=["accuracy"])
-# model_final.compile(loss="categorical_crossentropy", optimizer=optimizers.adadelta(lr=1.0, rho=0.95, epsilon=None, decay=0.0), metrics=['accuracy'])
+# model_final.compile(loss="categorical_crossentropy", optimizer=optimizers.SGD(lr=1, momentum=0.9), metrics=["accuracy"])
+model_final.compile(loss="categorical_crossentropy", optimizer=optimizers.adadelta(lr=1, rho=0.95, epsilon=None, decay=0), metrics=['accuracy'])
 
 # Initiate the train and test generators with data Augumentation
 train_datagen = ImageDataGenerator(
@@ -108,6 +101,10 @@ validation_generator = train_datagen.flow_from_directory(
 
 # early_stop = EarlyStopping(monitor='val_loss', patience=10, verbose=1)
 
+
+
+
+
 # Train the model
 model_final.fit_generator(
     train_generator,
@@ -116,10 +113,10 @@ model_final.fit_generator(
     validation_data=validation_generator,
     validation_steps=3000//batch_size,
     class_weight='auto',
-    shuffle=True,
-    callbacks=[TensorBoard(log_dir='mytensorboard')])
+    shuffle=True)
+#    callbacks=[TensorBoard(log_dir='mytensorboard')]
 #   callbacks=[early_stop],
 
 end = datetime.datetime.now()
 print(end-start)
-model_final.save_weights('C:\\Users\gavin\Desktop\model5.h5')
+model_final.save_weights('C:\\Users\gavin\Desktop\model_dw_linear_1.h5')
